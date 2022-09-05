54ee2d128805e80455ada4b24e68a506fd96923a
hello <leejungmin781@gmail.com>
2022-09-05 20:06:57 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "count.h"
+
+void count_(int num) {
+	for(int i=num;i>0;i--){
+		cout << "$\n" << 11 - i << endl;
+	}
+	cout << "Start!!\n" << endl;
+	cout << "2020314008 Jungmin Lee\n" << endl;
+}
